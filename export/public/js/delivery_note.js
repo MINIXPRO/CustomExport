135d3ef4ebfb4ed91c8ba223832ea6d100af6e6b
jigneshchuadhary757 <jigneshchaudhary757@gmail.com>
2026-03-26 11:07:13 +0530

    Fix: Net weight recalculation on validation

--- export/public/js/delivery_note.js
+++ export/public/js/delivery_note.js
@@ -7,6 +7,7 @@ frappe.ui.form.on("Delivery Note", {
         toggle_cif_total_by_currency(frm);
         toggle_sub_items_columns(frm);
         hide_items_rows(frm);
+        recalculate_dn_totals(frm);
 
         setup_items_grid_template_buttons(frm);
 
@@ -47,6 +48,7 @@ frappe.ui.form.on("Delivery Note", {
 
         apply_parent_values_from_sub_items(frm);
         calculate_dn_cif_totals(frm);
+        recalculate_dn_totals(frm);
 
         // Add Bank Charges item at the end when order type is Export
         // Commented out: reverting CIF difference adjustment via Bank Charges
@@ -451,10 +453,12 @@ frappe.ui.form.on("Delivery Note Item", {
     qty(frm, cdt, cdn) {
         calculate_net_weight(frm, cdt, cdn);
         calculate_cif_values(frm, cdt, cdn);
+        setTimeout(() => recalculate_dn_totals(frm), 150);
     },
 
     weight_per_unit(frm, cdt, cdn) {
         calculate_net_weight(frm, cdt, cdn);
+        setTimeout(() => recalculate_dn_totals(frm), 150);
     },
 
     custom_freight__insurance_(frm, cdt, cdn) {
@@ -558,6 +562,22 @@ frappe.ui.form.on("Delivery Note Sub Items", {
         calculate_sub_item_cif_values(frm, cdt, cdn);
     }
 });
+
+
+/************************************
+ * PARENT TOTALS: QTY + NET WEIGHT
+ ************************************/
+function recalculate_dn_totals(frm) {
+    let total_qty = 0;
+    let total_net_weight = 0;
+    (frm.doc.items || []).forEach(row => {
+        if (row.item_code === "Bank Charges") return;
+        total_qty += flt(row.qty);
+        total_net_weight += flt(row.custom_net_weight);
+    });
+    frm.set_value("total_qty", total_qty);
+    frm.set_value("total_net_weight", total_net_weight);
+}
 
 
 /************************************
